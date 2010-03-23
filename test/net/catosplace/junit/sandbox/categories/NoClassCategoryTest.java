8a49bd59027d7e506e212bd45a62a58f2b2d7c68
Peter Sellars <cato1971@xtra.co.nz>
2010-03-23 22:10:00 +1300

    Updated build and added logging and TestWatchman rule to base test class

--- test/net/catosplace/junit/sandbox/categories/NoClassCategoryTest.java
+++ test/net/catosplace/junit/sandbox/categories/NoClassCategoryTest.java
@@ -3,7 +3,13 @@
  */
 package net.catosplace.junit.sandbox.categories;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.junit.rules.TestWatchman;
+import org.junit.runners.model.FrameworkMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Test class that does not have a class level category.
@@ -13,8 +19,22 @@ import org.junit.Test;
  */
 public class NoClassCategoryTest {
 
+	
+	@Rule public MethodRule watchman = new TestWatchman() {
+		public void starting(FrameworkMethod method) {
+			logger.info("{} being run...", method.getName());
+		}
+	};
+	
+	final Logger logger = LoggerFactory.getLogger(NoClassCategoryTest.class);
+	
 	@Test
-	public void noCategoryTest() {
-		System.out.println("NoClassCategoryTest:noCategoryTest will be run...");
+	public void testA() {
+		
+	}
+	
+	@Test
+	public void testB() {
+
 	}
 }
